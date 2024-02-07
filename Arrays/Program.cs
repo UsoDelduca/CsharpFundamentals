6354b71497d61ccd57b14b90c88ff4a7ceecb97d
Rodolfo Delduca <usodelduca@gmail.com>
2024-02-08 12:17:26 +1300

    creating and array and assigning a new value to an index

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+namespace _07_Arrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] grades = [20, 15, 34, 40, 10]; //define the new array with length of 5
+      System.Console.WriteLine($"grade at index 0: {grades[0]}");
+      string input = Console.ReadLine();
+      grades[0] = int.Parse(input);
+      System.Console.WriteLine($"grade at index 0: {grades[0]}");
+    }
+  }
+}
