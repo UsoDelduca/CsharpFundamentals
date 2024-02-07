3891714984390149c24cdcb5937fdfb4d4400fe3
Rodolfo Delduca <usodelduca@gmail.com>
2024-02-08 12:20:51 +1300

    other ways to initialize arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -12,6 +12,15 @@ namespace _07_Arrays
       string input = Console.ReadLine();
       grades[0] = int.Parse(input);
       System.Console.WriteLine($"grade at index 0: {grades[0]}");
+
+      // another way to initialize an array
+      int[] newGradesA = {20, 15, 34, 40, 10 };
+
+      // another way
+      int[] newGradesB = new int[] {20, 15, 34, 40, 10};
+
+      System.Console.WriteLine($"New grade A at index 1: {newGradesA[1]}");
+      System.Console.WriteLine($"New grade B at index 2: {newGradesB[2]}");
     }
   }
 }
